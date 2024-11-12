a7ed719bf8deb057ac42ce5316437ee9d752241e
Kalyan Sriram <kalyan@coderkalyan.com>
2024-11-11 23:50:34 -0600

    plumbing

--- discrete.v
+++ discrete.v
@@ -1,12 +1,218 @@
 `default_nettype none
 
-`define DISCRETE_FORMAL
+// `define DISCRETE_FORMAL
 
-module core ();
-    alu alu (.i_op(), .i_op1(), .i_op2(), .i_sub(), .o_result());
-    comparator comp (.i_op1(), .i_op2());
-    shifter shifter (.i_op1(), .i_op2(), .o_result());
-    rf rf ();
+`define ALU_OP_ADD 0
+`define ALU_OP_OR 1
+`define ALU_OP_AND 2
+`define ALU_OP_XOR 3
+`define ALU_OP_SLT 4
+`define ALU_OP_SHIFT 5
+`define SHIFT_DIR_LEFT 0
+`define SHIFT_DIR_RIGHT 1
+`define RD_ALU 0
+`define RD_MEM 1
+`define RD_PC 2
+
+module core (
+    input  wire        i_clk,
+    input  wire        i_rst_n,
+    input  wire [31:0] o_imem_addr,
+    output wire [31:0] i_imem_rdata,
+    output wire [31:0] o_dmem_addr,
+    output wire [31:0] o_dmem_wdata,
+    input  wire [31:0] i_dmem_rdata
+);
+    reg [31:0] pc;
+    wire [31:0] next_pc;
+    always @(posedge i_clk) begin
+        if (!i_rst_n)
+            pc <= 32'h0;
+        else
+            pc <= next_pc;
+    end
+
+    assign o_imem_addr = pc;
+    wire [31:0] inst = i_imem_rdata;
+
+    wire [4:0] rs1_addr, rs2_addr, rd_addr;
+    wire rd_wen;
+    wire [2:0] rd_sel;
+    wire op1_sel, op2_sel;
+    wire [5:0] alu_op;
+    wire [1:0] shift_dir;
+    wire alu_sub, alu_sltu, shift_arith;
+    wire jump, branch;
+    wire branch_equal, branch_unsigned, branch_invert;
+    wire [31:0] imm;
+    decoder id (
+        .i_inst(inst),
+        .o_rs1_addr(rs1_addr), .o_rs2_addr(rs2_addr), .o_rd_addr(rd_addr),
+        .o_rd_wen(rd_wen), .o_rd_sel(rd_sel),
+        .o_op1_sel(op1_sel), .o_op2_sel(op2_sel),
+        .o_alu_op(alu_op), .o_alu_sub(alu_sub), .o_alu_sltu(alu_sltu),
+        .o_shift_dir(shift_dir), .o_shift_arith(shift_arith),
+        .o_jump(jump), .o_branch(branch),
+        .o_branch_equal(branch_equal),
+        .o_branch_unsigned(branch_unsigned),
+        .o_branch_invert(branch_invert),
+        .o_imm(imm)
+    );
+
+    wire [31:0] rs1_rdata, rs2_rdata, rd_wdata;
+    rf rf (
+        .i_clk(i_clk),
+        .i_rs1_addr(rs1_addr), .i_rs2_addr(rs2_addr), .i_rd_addr(rd_addr),
+        .i_rd_wen(rd_wen), .i_rd_wdata(rd_wdata),
+        .o_rs1_rdata(rs1_rdata), .o_rs2_rdata(rs2_rdata)
+    );
+
+    wire [31:0] op1 = op1_sel ? pc : rs1_rdata;
+    wire [31:0] op2 = op2_sel ? imm : rs2_rdata;
+
+    wire eq, lt, ltu;
+    comparator comp (
+        .i_op1(rs1_rdata), .i_op2(rs2_rdata),
+        .o_eq(eq), .o_lt(lt), .o_ltu(ltu)
+    );
+    wire take = (branch_equal ? eq : (branch_unsigned ? ltu : lt)) ^ branch_invert;
+
+    wire [31:0] alu_result;
+    alu alu (
+        .i_op(alu_op), .i_sub(alu_sub), .i_sltu(alu_sltu),
+        .i_dir(shift_dir), .i_arith(shift_arith),
+        .i_op1(op1), .i_op2(op2), .i_lt(lt), .i_ltu(ltu),
+        .o_result(alu_result)
+    );
+
+    assign o_dmem_addr = alu_result;
+    assign o_dmem_wdata = rs2_rdata;
+
+    wire [31:0] pc_inc = pc + 32'h4;
+    wire pc_sel = jump || (branch && take);
+    assign next_pc = pc_sel ? alu_result : pc_inc;
+
+    wire [31:0] rd_sel_alu = {32{rd_sel[`RD_ALU]}};
+    wire [31:0] rd_sel_mem = {32{rd_sel[`RD_MEM]}};
+    wire [31:0] rd_sel_pc  = {32{rd_sel[`RD_PC]}};
+    assign rd_wdata = (alu_result & rd_sel_alu) | (32'h0 & rd_sel_mem) | (pc_inc & rd_sel_pc);
+endmodule
+
+module decoder (
+    input  wire [31:0] i_inst,
+    output wire [4:0]  o_rs1_addr,
+    output wire [4:0]  o_rs2_addr,
+    output wire [4:0]  o_rd_addr,
+    output wire        o_op1_sel,
+    output wire        o_op2_sel,
+    output wire [5:0]  o_alu_op,
+    output wire        o_alu_sub,
+    output wire        o_alu_sltu,
+    output wire [1:0]  o_shift_dir,
+    output wire        o_shift_arith,
+    output wire        o_rd_wen,
+    output wire [2:0]  o_rd_sel,
+    output wire        o_jump,
+    output wire        o_branch,
+    output wire        o_branch_equal,
+    output wire        o_branch_unsigned,
+    output wire        o_branch_invert,
+    output wire [31:0] o_imm
+);
+    wire [4:0] opcode = i_inst[6:2];
+    wire [4:0] rs1 = i_inst[19:15];
+    wire [4:0] rs2 = i_inst[24:20];
+    wire [4:0] rd = i_inst[11:7];
+    wire [2:0] funct3 = i_inst[14:12];
+    wire [6:0] funct7 = i_inst[31:25];
+
+    // major opcode selection
+    wire op_load     = opcode == 5'b00000;
+    wire op_misc_mem = opcode == 5'b00011;
+    wire op_op_imm   = opcode == 5'b00100;
+    wire op_auipc    = opcode == 5'b00101;
+    wire op_store    = opcode == 5'b01000;
+    wire op_amo      = opcode == 5'b01011;
+    wire op_op       = opcode == 5'b01100;
+    wire op_lui      = opcode == 5'b01101;
+    wire op_branch   = opcode == 5'b11000;
+    wire op_jalr     = opcode == 5'b11001;
+    wire op_jal      = opcode == 5'b11011;
+    wire op_system   = opcode == 5'b11100;
+
+    wire alu_funct7 = funct7 == 7'b0100000;
+    wire alu_add  = funct3 == 3'b000;
+    wire alu_sl   = funct3 == 3'b001;
+    wire alu_sr   = funct3 == 3'b101;
+    wire alu_slt  = funct3 == 3'b010;
+    wire alu_sltu = funct3 == 3'b011;
+    wire alu_xor  = funct3 == 3'b100;
+    wire alu_or   = funct3 == 3'b110;
+    wire alu_and  = funct3 == 3'b111;
+
+    assign o_rd_wen = op_load || op_op_imm || op_auipc || op_op || op_lui || op_jalr || op_jal;
+    assign o_rd_sel[`RD_ALU] = !o_rd_sel[`RD_MEM] && ~o_rd_sel[`RD_PC];
+    assign o_rd_sel[`RD_MEM] = op_load;
+    assign o_rd_sel[`RD_PC] = op_jal || op_jalr;
+
+    assign o_op1_sel = op_branch || op_auipc || op_jal;
+    assign o_op2_sel = op_load || op_op_imm || op_auipc || op_store || op_lui || op_branch || op_jal;
+
+    wire op_inst = op_op_imm || op_op;
+    assign o_alu_op[`ALU_OP_ADD] = (op_inst && alu_add) || op_load || op_auipc || op_store || op_lui || op_branch || op_jalr || op_jal;
+    assign o_alu_op[`ALU_OP_OR] = op_inst && alu_or;
+    assign o_alu_op[`ALU_OP_AND] = op_inst && alu_and;
+    assign o_alu_op[`ALU_OP_XOR] = op_inst && alu_xor;
+    assign o_alu_op[`ALU_OP_SLT] = op_inst && (alu_slt || alu_sltu);
+    assign o_alu_op[`ALU_OP_SHIFT] = op_inst && (alu_sl || alu_sr);
+    assign o_alu_sub = op_inst && alu_add && alu_funct7;
+    assign o_alu_sltu = alu_sltu;
+
+    assign o_shift_dir[`SHIFT_DIR_LEFT] = alu_sl;
+    assign o_shift_dir[`SHIFT_DIR_RIGHT] = alu_sr;
+    assign o_shift_arith = alu_funct7;
+
+    assign o_jump = op_jal || op_jalr;
+    assign o_branch = op_branch;
+    assign o_branch_equal = funct3[2];
+    assign o_branch_unsigned = funct3[1];
+    assign o_branch_invert = funct3[0];
+
+    // immediate decoding
+    wire format_r = op_op;
+    wire format_i = op_op_imm || op_jalr || op_load;
+    wire format_s = op_store;
+    wire format_b = op_branch;
+    wire format_u = op_lui || op_auipc;
+    wire format_j = op_jal;
+
+    wire format_sb = format_s | format_b;
+    wire format_ij = format_i | format_j;
+    wire format_uj = format_u | format_j;
+
+    assign o_imm[0] = (format_s & i_inst[7]) | (format_i && i_inst[20]);
+    assign o_imm[4:1] = ({4{format_sb}} & i_inst[11:8]) | ({4{format_ij}} & i_inst[24:21]);
+    assign o_imm[10:5] = {6{!format_u}} & i_inst[30:25];
+    assign o_imm[11] = format_b ? i_inst[7] : (format_j ? i_inst[20] : (format_u ? 1'b0 : i_inst[31]));
+    assign o_imm[19:12] = format_uj ? i_inst[19:12] : {8{i_inst[31]}};
+    assign o_imm[30:20] = format_u ? i_inst[30:20] : {11{i_inst[31]}};
+    assign o_imm[31] = i_inst[31];
+
+    assign o_rs1_addr = rs1;
+    assign o_rs2_addr = rs2;
+    assign o_rd_addr = rd;
+
+`ifdef DISCRETE_FORMAL
+    always @(*) begin
+        assert ((op_load + op_misc_mem + op_op_imm + op_auipc + op_store + op_amo + op_op + op_lui + op_branch + op_jalr + op_jal + op_system) == 1);
+        assert ((o_alu_op[0] + o_alu_op[1] + o_alu_op[2] + o_alu_op[3] + o_alu_op[4]) == 1);
+        if (!o_alu_op[`ALU_OP_ADD])
+            assert (!o_alu_sub);
+        if (o_alu_op[`ALU_OP_SHIFT])
+            assert ((o_shift_dir[0] + o_shift_dir[1]) == 1);
+        assert ((o_rd_sel[0] + o_rd_sel[1] + o_rd_sel[2]) == 1);
+    end
+`endif
 endmodule
 
 module sram (
@@ -41,12 +247,12 @@ module rf (
     wire [4:0] addr1 = dir ? i_rd_addr : i_rs2_addr;
 
     wire wen = dir && i_rd_wen;
-    wire [31:0] rs1_zero = {32{!|i_rs1_addr}};
-    wire [31:0] rs2_zero = {32{!|i_rs2_addr}};
+    wire [31:0] rs1_zero = {32{!(|i_rs1_addr)}};
+    wire [31:0] rs2_zero = {32{!(|i_rs2_addr)}};
 
     wire [31:0] rs1_rdata, rs2_rdata;
-    sram bank0 [3:0] (.i_addr(addr0), .i_wen(wen), .i_data(i_rd_wdata), .o_data(rs1_rdata));
-    sram bank1 [3:0] (.i_addr(addr1), .i_wen(wen), .i_data(i_rd_wdata), .o_data(rs2_rdata));
+    // sram bank0 [3:0] (.i_addr(addr0), .i_wen(wen), .i_data(i_rd_wdata), .o_data(rs1_rdata));
+    // sram bank1 [3:0] (.i_addr(addr1), .i_wen(wen), .i_data(i_rd_wdata), .o_data(rs2_rdata));
 
     // on read, the RAMs will drive the data lines with read data
     // no need to gate this for read, but we mask so that reads to
@@ -84,41 +290,53 @@ module rca (
 endmodule
 
 module alu (
-    // 0 = add
-    // 1 = or
-    // 2 = and
-    // 3 = xor
-    input  wire [3:0]  i_op,
+    input  wire [5:0]  i_op,
     // assert to enable subtraction mode on the adder
     input  wire [31:0] i_op1,
     input  wire [31:0] i_op2,
+    input  wire        i_lt,
+    input  wire        i_ltu,
     input  wire        i_sub,
+    input  wire        i_sltu,
+    input  wire [1:0]  i_dir,
+    input  wire        i_arith,
     output wire [31:0] o_result
 );
     wire [31:0] or_result = i_op1 | i_op2;
+    wire slt_result = i_sltu ? i_ltu : i_lt;
     wire [31:0] add_result, xor_result, and_result;
     rca adder (
         .i_a(i_op1), .i_b(i_op2 ^ {32{i_sub}}), .i_cin(i_sub),
         .o_sum(add_result), .o_xor(xor_result), .o_and(and_result)
     );
 
-    wire [31:0] op_add = {32{i_op[0]}};
-    wire [31:0] op_or  = {32{i_op[1]}};
-    wire [31:0] op_and = {32{i_op[2]}};
-    wire [31:0] op_xor = {32{i_op[3]}};
-    assign o_result = (add_result & op_add) | (or_result & op_or) | (and_result & op_and) | (xor_result & op_xor);
+    wire [31:0] shift_result;
+    shifter shifter (
+        .i_op1(i_op1), .i_op2(i_op2[4:0]),
+        .i_dir(i_dir), .i_arith(i_arith),
+        .o_result(shift_result)
+    );
+
+    wire [31:0] op_add   = {32{i_op[`ALU_OP_ADD]}};
+    wire [31:0] op_or    = {32{i_op[`ALU_OP_OR]}};
+    wire [31:0] op_and   = {32{i_op[`ALU_OP_AND]}};
+    wire [31:0] op_xor   = {32{i_op[`ALU_OP_XOR]}};
+    wire [31:0] op_shift = {32{i_op[`ALU_OP_SHIFT]}};
+    wire        op_slt   = i_op[`ALU_OP_SLT];
+    assign o_result = (add_result & op_add) | (or_result & op_or) | (and_result & op_and) | (xor_result & op_xor) | ({31'h0, slt_result & op_slt}) | (shift_result & op_shift);
 
 `ifdef DISCRETE_FORMAL
     always @(*) begin
-        assume ((i_op[0] + i_op[1] + i_op[2] + i_op[3]) == 1);
+        assume ((i_op[0] + i_op[1] + i_op[2] + i_op[3] + i_op[4]) == 1);
         if (!i_op[0])
             assume (!i_sub);
 
         case (1'b1)
-            i_op[0]: assert (o_result == (i_sub ? (i_op1 - i_op2) : (i_op1 + i_op2)));
-            i_op[1]: assert (o_result == (i_op1 | i_op2));
-            i_op[2]: assert (o_result == (i_op1 & i_op2));
-            i_op[3]: assert (o_result == (i_op1 ^ i_op2));
+            i_op[`ALU_OP_ADD]: assert (o_result == (i_sub ? (i_op1 - i_op2) : (i_op1 + i_op2)));
+            i_op[`ALU_OP_OR]: assert (o_result == (i_op1 | i_op2));
+            i_op[`ALU_OP_AND]: assert (o_result == (i_op1 & i_op2));
+            i_op[`ALU_OP_XOR]: assert (o_result == (i_op1 ^ i_op2));
+            i_op[`ALU_OP_SLT]: assert (o_result == ({31'h0, i_sltu ? i_ltu : i_lt}));
         endcase
     end
 `endif
@@ -164,15 +382,11 @@ module comparator (
     input  wire [31:0] i_op1,
     input  wire [31:0] i_op2,
     output wire        o_eq,
-    output wire        o_ne,
     output wire        o_lt,
-    output wire        o_ltu,
-    output wire        o_ge,
-    output wire        o_geu
+    output wire        o_ltu
 );
     // equality
     assign o_eq = &(i_op1 ~^ i_op2);
-    assign o_ne = !o_eq;
 
     // less than
     wire [31:0] ltu_bits;
@@ -180,18 +394,11 @@ module comparator (
     assign o_lt = (i_op1[31] == i_op2[31]) ? ltu_bits[30] : i_op1[31];
     assign o_ltu = ltu_bits[31];
 
-    // greater than equal
-    assign o_ge = !o_lt | o_eq;
-    assign o_geu = !o_ltu | o_eq;
-
 `ifdef DISCRETE_FORMAL
     always @(*) begin
         assert (o_eq == (i_op1 == i_op2));
-        assert (o_ne == (i_op1 != i_op2));
-        assert (o_ltu == (i_op1 < i_op2));
         assert (o_lt == ($signed(i_op1) < $signed(i_op2)));
-        assert (o_geu == (i_op1 >= i_op2));
-        assert (o_ge == ($signed(i_op1) >= $signed(i_op2)));
+        assert (o_ltu == (i_op1 < i_op2));
     end
 `endif
 endmodule
